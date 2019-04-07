0bda1e5fdd2a7ee53288f974cda3f85d3f2ba5df
loetek <jdwheeler0@gmail.com>
2019-04-07 17:28:53 -0500

    Details index all working fine. Need to add edit and delete.

--- WebAppTest_1/Controllers/StudentsController.cs
+++ WebAppTest_1/Controllers/StudentsController.cs
@@ -37,12 +37,12 @@ namespace WebAppTest_1.Controllers
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-            SELECT s.Id
+            SELECT s.Id,
                 s.FirstName,
                 s.LastName,
                 s.SlackHandle,
                 s.CohortId
-            FROM Student s;";
+            FROM Student s";
                     SqlDataReader reader = cmd.ExecuteReader();
 
                     List<Student> students = new List<Student>();
@@ -73,7 +73,32 @@ namespace WebAppTest_1.Controllers
         //       // GET: Students / Details / 5
         public ActionResult Details(int id)
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT s.Id, s.FirstName, s.LastName, s.Slackhandle, s.CohortId FROM Student s WHERE id = @id;";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Student student = null;
+                    if (reader.Read())
+                    {
+                        student = new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                            CohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
+                        };
+                    }
+
+                    reader.Close();
+                    return View(student);
+                }
+            }
         }
 
         //        //GET: Students / Create
